e06f5c77a02e5fe9f44fc71f996174865ddd4927
Marcelo Antonio <maeceloacm1998@gmail.com>
2024-10-01 14:58:35 -0300

    refactor: adjust comment

--- src/routes/migrations.routes.ts
+++ src/routes/migrations.routes.ts
@@ -7,16 +7,11 @@ import { Router } from "express";
 
 function migrationRoutes(route: Router) {
   /**
-   * Essa chamada retorna o servico de emergencia mais proximo.
-   * @param latitude: a latitude do usuário
-   * @param longitude: a longitude do usuário
-   * @param message: a mensagem que o usuario digitou
-   *
-   * Exemplo de uso:
-   * /api/search/emergency?latitude=-19.9198&longitude=-43.9386&message=acidente
+   * Todas essas rotas sao responsaveis para migrar items de policia, hospita e fire. 
+   * Hoje, caso queria adicionar alguma policia ou hospital, basta entrar nessa rota e enviar um array dos items que voce deseja mandar,
+   * mas voce vai precisar fazer alguns ajustes.
    */
   route.route("/api/migrations/hospital").post(getHospitalMigrations);
-
   route.route("/api/migrations/policy").post(getPolicyMigrations);
   route.route("/api/migrations/fire").post(getFireMigrations);
 }
